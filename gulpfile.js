c1533d652df127e96fe5601aa8b8b3969f940f3c
jtparrett <jtparrett@me.com>
2017-10-18 10:43:21 +0100

    gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const autoprefixer = require('gulp-autoprefixer')
+
+gulp.task('styles', function(){
+  gulp.src('ui/stylesheets/*.{sass,scss,css}')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(gulp.dest('application/themes/giantjelly/app'))
+})
+
+gulp.task('default', function(){
+  gulp.watch('ui/stylesheets/*.{sass,scss,css}', ['styles'])
+})
