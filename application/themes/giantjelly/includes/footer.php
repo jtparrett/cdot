1243a3d958c81882f0d288622d85f5c48cdb072a
jtparrett <jtparrett@me.com>
2017-12-03 13:49:21 +0000

    footer work

--- application/themes/giantjelly/includes/footer.php
+++ application/themes/giantjelly/includes/footer.php
@@ -1,7 +1,8 @@
 <footer class="footer">
   <img src="http://picsum.photos/1400/788?image=973" alt="" class="footer__image" />
-  
-  <div class="wrapper">
+
+  <div class="wrapper wrapper--slim">
+    <h1 class="footer__title">Proven results</h1>
     <p class="footer__copy">&copy; Copyright 2017. Cdot Entertainment</p>
   </div>
 </footer>
