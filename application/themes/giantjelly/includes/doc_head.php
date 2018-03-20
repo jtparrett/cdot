8bf481ddc1a1775ccc5d72c4673972c3719d884d
giantjelly <nwalker@rawnet.com>
2018-03-20 20:02:50 -0300

    banner area

--- application/themes/giantjelly/includes/doc_head.php
+++ application/themes/giantjelly/includes/doc_head.php
@@ -1,15 +1,19 @@
-<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+<?php use Concrete\Core\Area\Area;
+
+defined('C5_EXECUTE') or die("Access Denied."); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <?php Loader::element('header_required'); ?>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui" />
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i,700,700i" rel="stylesheet">
-  <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/app/application.css" />
+	<?php Loader::element('header_required'); ?>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui"/>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,600,600i,700,700i" rel="stylesheet">
+    <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/app/application.css"/>
 </head>
 <body>
-  <div class="page">
-    <?php 
-      $this->inc('includes/header.php');
-      $this->inc('includes/banner.php');
-    ?>
+<div class="page">
+	<?php
+	$this->inc('includes/header.php');
+	$bannerArea = new Area('banner');
+	$bannerArea->display();
+	?>
